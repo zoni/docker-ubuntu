2cf4caee155b7ec5a6254839b6009b0aff02136f
Nick Groenen <nick@byte.nl>
2015-12-07 17:07:18 +0100

    Add ubuntu 15.10 as latest

---
+++ 15.10/bin/build.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -e
+set -x
+
+echo 'debconf debconf/frontend select noninteractive' | debconf-set-selections
+aptinstall="apt-get install --no-install-recommends --yes"
+
+# Remove cached deb archives after package installation
+echo 'DPkg::Post-Invoke {"/bin/rm -f /var/cache/apt/archives/*.deb || true";};' > /etc/apt/apt.conf.d/99-no-cache
+
+# Set UTF8 locale
+echo LC_ALL="en_US.utf8" >> /etc/environment
+locale-gen en_US.UTF-8
+
+apt-get update
+apt-get dist-upgrade --no-install-recommends --yes
+$aptinstall sudo vim-tiny
+
+rm -rf /var/lib/apt/lists/*
